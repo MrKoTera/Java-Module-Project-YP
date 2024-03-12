357673f1d70c015a59d522401e2b51e67229359a
Nikita Butov <kotbbmewke@gmail.com>
2024-03-12 10:02:31 +0300

    Готово

--- src/main/java/ProductList.java
+++ src/main/java/ProductList.java
@@ -4,23 +4,28 @@ import java.util.Scanner;
 class ProductList {
     public static ArrayList<Product> inputOrders() {
         ArrayList<Product> products = new ArrayList<>();
-        try (Scanner scanner = new Scanner(System.in)) {
+
             while (true) {
-                System.out.println("Введите название товара:");
-                String orderName = scanner.next();
-                System.out.println("Введите стоимость товара:");
-                double orderPrice = scanner.nextFloat();
-                if(orderPrice <= 0) {
-                    System.out.println("Некорректная сумма!");
-                    continue;
+                try {
+                    Scanner scanner = new Scanner(System.in);
+                    System.out.println("Введите название товара:");
+                    String orderName = scanner.next();
+                    System.out.println("Введите стоимость товара:");
+                    double orderPrice = scanner.nextFloat();
+                    if(orderPrice <= 0) {
+                        System.out.println("Некорректная сумма!");
+                        continue;
+                    }
+
+                    products.add(new Product(orderName, orderPrice));
+
+                    System.out.println("Товар успешно добавлен!\n\nХотите добавить ещё товар?\n1. Для продолжения напишите любой символ\n2. Для отмены напишите \"Завершить\" в любом регистре.");
+                    String action = scanner.next();
+                    if(action.equalsIgnoreCase("Завершить")) return products;
+                } catch (Exception e) {
+                    System.out.println("Ошибка: некорректный ввод");
                 }
 
-                products.add(new Product(orderName, orderPrice));
-
-                System.out.println("Товар успешно добавлен!\n\nХотите добавить ещё товар?\n1. Для продолжения напишите любой символ\n2. Для отмены напишите \"Завершить\" в любом регистре.");
-                String action = scanner.next();
-                if(action.equalsIgnoreCase("Завершить")) return products;
             }
-        }
     }
 }
